fcad3fac419ffbdadde31566513895bfcc39bb60
Ernest V <ernest.viol@gmail.com>
2026-09-12 18:23:44 -0700

    feat: Add tests to confirm we can reach all routes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,24 @@
-const Navbar = () => {};
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/explore'>Explore</NavLink>
+        </li>
+        <li>
+          <NavLink to='/play'>Play</NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
--- client/src/components/Navbar/Navbar.test.jsx
+++ client/src/components/Navbar/Navbar.test.jsx
@@ -1,4 +1,39 @@
-test.todo('Can navigate to Home page');
-test.todo('Can navigate to Explore');
-test.todo('Can navigate to Play');
-test.todo('Can navigate to About');
+import { render, screen } from '@testing-library/react';
+import vi, { beforeAll } from 'vitest';
+import { MemoryRouter } from 'react-router';
+import Navbar from './Navbar.jsx';
+
+describe('Navigation tests', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
+  });
+  test('component mounts', () => {
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+  });
+
+  test('navigate links exist', () => {
+    expect(screen.getByRole('link', { name: /home/i })).toHaveAttribute(
+      'href',
+      '/',
+    );
+
+    expect(screen.getByRole('link', { name: /explore/i })).toHaveAttribute(
+      'href',
+      '/explore',
+    );
+
+    expect(screen.getByRole('link', { name: /play/i })).toHaveAttribute(
+      'href',
+      '/play',
+    );
+
+    expect(screen.getByRole('link', { name: /about/i })).toHaveAttribute(
+      'href',
+      '/about',
+    );
+  });
+});
