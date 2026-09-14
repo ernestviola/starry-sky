6ce0de8371371dbafa774895322236d665ad17ac
Ernest V <ernest.viol@gmail.com>
2026-09-14 16:50:46 -0700

    feat: Fix hero responsizeness

--- client/src/routes/Home/Home.jsx
+++ client/src/routes/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './home.module.css';
+import { NavLink } from 'react-router';
 
 import StarMap from '../../components/StarMap/index.jsx';
 
@@ -9,8 +10,10 @@ const Home = () => {
     <div className={styles.home}>
       <section className={styles.hero}>
         <h1>Know your sky?</h1>
-        <p>Compete against others and see who knows their sky the best!</p>
-        <button>Play now</button>
+        <div className={styles.heroActions}>
+          <p>Compete against others and see who knows their sky the best!</p>
+          <NavLink to='/play'>Play now</NavLink>
+        </div>
       </section>
       <section className={styles.starMap}>
         <StarMap />
